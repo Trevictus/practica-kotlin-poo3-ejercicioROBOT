29b294f324c4b610a563e44346a24955beaa878c
vgomtej353 <gtvictor@iesrafaelalberti.es>
2025-02-04 15:52:56 +0100

    array<array<int>>

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,15 @@
 fun main(){
     val robot1 = Robot("R2D2")
 
-    val camino1: Array<Int> = arrayOf(-10, -5, 2, 4, -8)
-
-    robot1.mover(camino1)
-    println(robot1)
+    val camino1: Array<Array<Int>> = arrayOf(
+        arrayOf(1, -5, 0, -9),
+        arrayOf(3, 3, 5, 6, 1, 0, 0, -7),
+        arrayOf(2, 1, 0, -1, 1, 1, -4),
+        arrayOf(),
+        arrayOf(3, 5)
+    )
+    for(pasos in camino1) {
+        robot1.mover(pasos)
+        println(robot1)
+    }
 }
